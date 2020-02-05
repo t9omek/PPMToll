72563f19bbd4a870dc7e455421aa648bf75d871e
tom <t9omek@gmail.com>
2020-02-05 22:01:03 +0100

    first commit

---
+++ src/test/java/io/agileintekligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintekligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
